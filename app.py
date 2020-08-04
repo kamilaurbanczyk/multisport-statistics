25ef6f838709bbd89e8ccefc49338e1872c7d8ed
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2020-08-04 13:34:50 +0200

    add columns to db
    Added 2 columns (training-rate and classes-rate) to db with
    flask-migrate

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 class Multisport(db.Model):
     id = db.Column(db.Integer, primary_key= True)
@@ -16,7 +22,8 @@ class Multisport(db.Model):
     duration = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
-
+    classes_rate = db.Column(db.Integer, nullable=True)
+    training_rate = db.Column(db.Integer, nullable=True)
 
 from views import *
 
