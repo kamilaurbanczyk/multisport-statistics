e4b0676c73d30a165d418c7f0df2798f29d5f58c
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2020-08-25 14:12:13 +0200

    psycopg2 exception edit

--- views.py
+++ views.py
@@ -10,7 +10,7 @@ from passlib.hash import sha256_crypt
 from operator import itemgetter
 from datetime import datetime
 from functools import wraps
-from psycopg2 import errors
+import psycopg2
 
 if ENV == 'dev':
     engine = create_engine('mysql://root:23101996Kamila@localhost/multisport', echo=False)
